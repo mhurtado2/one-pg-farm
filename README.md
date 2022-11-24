87406edc007e26269b6a9bcaf6e4464e85c87874
Shane <mhurtado1@berklee.edu>
2022-11-24 14:51:32 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# one-pg-farm
---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+
+
+const types = ["Soybean", "Corn", "Asparagus", "Wheat", "Potato", "Sunflower"]
+
+const createPlan = () => {
+    const plan = []
+
+    for (let index = 0; index < 3; index++) {
+        const row = []
+        for (let j = 0; j < 6; j++) {
+            row.push(crop.next().value)
+        }
+        plan.push(row)
+    }
+
+    return plan
+}
+
+const crop = function* () {
+    while (true) {
+        const typeIdx = Math.floor(Math.random() * types.length)
+        yield types[typeIdx]
+    }
+}()
+
+const yearlyPlan = createPlan()
+console.log(yearlyPlan)
